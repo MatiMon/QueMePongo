7f2305bbe2775aa949b35df1626b250302c9d131
mati <matimon@frba.utn.edu.ar>
2021-04-30 17:03:33 -0300

    Modificada la clase Borrador para que no rompa encapsulamiento de Prenda y agregado el metodo crearPrenda

--- src/main/java/Borrador.java
+++ src/main/java/Borrador.java
@@ -1,7 +1,33 @@
 public class Borrador {
-    Prenda prenda;
+    TipoPrenda tipoPrenda;
+    Material material;
+    Color colorPrimario;
+    Color colorSecundario;
+    Trama trama;
 
-    public Borrador(Prenda prenda){
-        this.prenda = prenda;
+    public Prenda crearPrenda(TipoPrenda tipoPrenda, Material material, Color colorPrimario, Color colorSecundario, Trama trama){
+        Prenda prenda = new Prenda(tipoPrenda, material, colorPrimario, colorSecundario, trama);
+        return prenda;
+    }
+
+    //Setters
+    public void setTipoPrenda(TipoPrenda tipoPrenda) {
+        this.tipoPrenda = tipoPrenda;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public void setColorPrimario(Color colorPrimario) {
+        this.colorPrimario = colorPrimario;
+    }
+
+    public void setColorSecundario(Color colorSecundario) {
+        this.colorSecundario = colorSecundario;
+    }
+
+    public void setTrama(Trama trama) {
+        this.trama = trama;
     }
 }
