651c16c638ec7a310c699750a90f524d69f399b1
mati <matimon@frba.utn.edu.ar>
2021-06-10 21:54:57 -0300

    agrego enum criterio

---
+++ src/main/java/Criterio.java
@@ -0,0 +1,3 @@
+public enum Criterio {
+  VIAJE, ENTRECASA
+}
--- src/main/java/Guardarropas.java
+++ src/main/java/Guardarropas.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class Guardarropas {
   private List<Prenda> prendas;
-  //Criterio criterio;
+  Criterio criterio;
 
   public Guardarropas(List<Usuario> propietarios) {
   }
